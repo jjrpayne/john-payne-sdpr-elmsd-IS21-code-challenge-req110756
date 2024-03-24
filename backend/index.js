86822481e521a0310e63da50e52e97a15044ba7e
jjrpayne <jjrpayne@hotmail.com>
2024-03-23 22:32:32 -0700

    initialized express app

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE"
+  );
+    next();
+});
+
+router = express.Router();
+app.use(router);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
